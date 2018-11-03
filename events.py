ba86a954ce2d06ca096d86f028c529830af251d4
Nathan Justinian <nathanj758@gmail.com>
2018-11-02 23:28:36 -0400

    Reworked the event handling system

--- events.py
+++ events.py
@@ -1,15 +1,88 @@
-from blinker import signal
-# from enum import Enum
 
-class Events:
-    PLAYER_CONNECTED = signal("playerConnected")
-    PLAYER_DISCONNECT = signal("playerDisconnect")
-    PLAYER_CONNECTING = signal("playerConnecting")
-    PLAYER_SPECTATE = signal("playerSpectate")
-    PLAYER_JOIN = signal("playerJoin")
-    CHATMSG = signal("chatMsg")
-    DEBUG = signal("debug")
-# TODO: add team join event
-# TODO: add quad event
-# TODO: add flag capture event
-# TODO: add flag score event
+import re
+
+from abc import ABC, abstractmethod
+
+class Event(ABC):
+    _handlers = []
+
+    @classmethod
+    def addHandler(cls, func):
+        cls._handlers.append(func)
+
+    @classmethod
+    def removeHandler(cls, func):
+        cls._handlers.remove(func)
+
+    @classmethod
+    def getEventHandlers(cls):
+        return cls._handlers
+
+    @classmethod
+    def connect(cls, func):
+        def wrapper(*args, **kwargs):
+            func(*args, **kwargs)
+        cls.addHandler(func)
+
+    @abstractmethod
+    def check(*args, **kwargs):
+        pass
+
+    def fire(*args, **kwargs):
+        for func in Event._handlers:
+            func(*args, **kwargs)
+    def __new__(cls):
+        _events.append(cls)
+
+class PlayerConnectingEvent(Event):
+	@classmethod
+	def check(cls, line):
+		return line.endswith(" is connecting...")
+
+
+class PlayerConnectEvent(Event):
+	@classmethod
+	def check(cls, line):
+		return line.endswith(" connected\x1b[m")
+
+class PlayerSpectateEvent(Event):
+	@classmethod
+	def check(cls, line):
+		return line.endswith(" is now spectating\x1b[m")
+
+class PlayerJoinEvent(Event):
+	@classmethod
+	def check(cls, line):
+		return line.endswith(" is now playing\x1b[m")
+
+class PlayerDisconnectEvent(Event):
+	@classmethod
+	def check(cls, line):
+		return line.endswith(" disconnected\x1b[m")
+
+
+class ChatMsgEvent(Event):
+    _chatMsg = re.compile("^(?:\x1b\[m|\x1b\[\d\;\d+m)*(.*?)\x1b\[m: (.*)")
+
+    @classmethod
+    def check(cls, line):
+        return True if _extractMsg.findall(line) else False
+
+class FragEvent(Event):
+    _fragMsg = re.compile("^(?:\x1b\[\d\;\d+m)?(.*?)\x1b.*?\x1b\[m(?:\x1b\[\d\;\d+m)?(.*?)\x1b.*?(?:'s (.*?))? \(near (.*)\)")
+
+    @classmethod
+    def check(cls, line):
+        pass
+
+class SuicideEvent(Event):
+    _suicideMsg = re.compile("^(?:\x1b\[\d\;\d+m)?(.*?)\x1b.*? (\w+) \(near (.*?)\)")
+
+    @classmethod
+    def check(cls, line):
+        pass9
+
+class DebugEvent(Event):
+    @classmethod
+    def check(cls):
+        pass
