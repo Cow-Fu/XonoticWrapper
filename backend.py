36b0fdb6bf2bb910bb6c768f9f2e8f99ce7feae3
Nathan Justinian <nathanj758@gmail.com>
2018-11-15 00:16:49 -0500

    impl some new changes

--- backend.py
+++ backend.py
@@ -1,58 +1,37 @@
 from subprocess import Popen, PIPE
 from events import Event, DebugEvent, ChatMsgEvent
+from xonIO import startXonoticProcess, outputStream
 import PlayerManager
 import re
 
-regex = re.compile("^(?:\x1b\[m|\x1b\[\d\;\d+m)*(.*?)\x1b\[m: (.*)")
+# regex = re.compile("^(?:\x1b\[m|\x1b\[\d\;\d+m)*(.*?)\x1b\[m: (.*)")
 
-def startProcess(cmd):
-    return Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True, bufsize=1)
+# def outputStream():
+#     lines = []
+#     with open("outputCmd.txt", "rb") as f:
+#         lines = f.read().decode("utf-8").split("\n")
+#
+#     for x in lines:
+#         yield x
+# writer, process = startXonoticProcess()
+activeEvents = list(filter(lambda x: not x._exclude, Event.__subclasses__()))
+passiveEvents = list(filter(lambda x: x._exclude, Event.__subclasses__()))
 
-# def outputStream(process):
-#     for line in process.stdout:
-#         yield line.strip()
+print(activeEvents)
+print(passiveEvents)
+exit()
 
-def writeToProcess(process):
-    def write(data, flush=True):
-        if flush:
-            data += "\n"
-        process.stdin.write(data)
-    return write
 
-cmd = "/home/nathan/xonoticgit/xonotic/all run sdl"
-# p = startProcess(cmd)
-# write = writeToProcess(process=p)
-
-def outputStream():
-    lines = []
-    with open("outputCmd.txt", "rb") as f:
-        lines = f.read().decode("utf-8").split("\n")
-
-    for x in lines:
-        yield x
-
-events = Event.__subclasses__()
-# for x in events:
-#     if x._exclude == True:
-#         print(x)
-#         exit()
-# events.remove(debug)
-for x in events:
-    if x is DebugEvent:
-        debug = x
-        break
-events.remove(debug)
-
-for line in outputStream():
+for line in outputStream(process):
     if line == "":
         continue
-    activeEvents = []
+    triggeredEvents = []
     for e in events:
         if e.check(line):
-            activeEvents.append(e)
+            triggeredEvents.append(e)
 
-    if activeEvents:
-        for e in activeEvents:
+    if triggeredEvents:
+        for e in triggeredEvents:
             print(e)
             e.fire(line)
             print("{}{}".format(line, e))
