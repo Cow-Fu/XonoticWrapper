6ca47c0e6a69effab965dcc54bbe5f33da99bfec
Nathan Justinian <nathanj758@gmail.com>
2018-11-17 01:24:55 -0500

    Added basic multithreading

--- xonIO.py
+++ xonIO.py
@@ -1,5 +1,15 @@
 from events import CmdEvent
 from subprocess import Popen, PIPE
+from threading import Thread
+
+class XonIO(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.collection = deque([])
+
+    def run(self, process):
+        for line in process:
+            self.collection.append(line)
 
 def startProcess(cmd):
     return Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True, bufsize=1)
@@ -17,7 +27,6 @@ def writeToProcess(process):
         if flush:
             data += "\n"
         process.stdin.write(data)
-        CmdEvent._last_cmd = data
     return write
 
 _cmd = "/home/nathan/xonoticgit/xonotic/all run sdl"
