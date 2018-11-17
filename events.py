45e074424a7c716b6884ed738336630409606143
Nathan Justinian <nathanj758@gmail.com>
2018-11-17 17:44:36 -0500

    fixed bug with event system

--- events.py
+++ events.py
@@ -3,7 +3,14 @@ import re
 
 from abc import ABC, abstractmethod
 
-class Event(metaclass=LittleMeta):
+# class Singleton(type):
+#     _instances = {}
+#     def __call__(cls, *args, **kwargs):
+#         if cls not in cls._instances:
+#             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+#         return cls._instances[cls]
+
+class Event(ABC):
     _exclude = False
     _has_handler = False
 
@@ -26,7 +33,7 @@ class Event(metaclass=LittleMeta):
         cls.addHandler(func)
         return wrapper
 
-    # @abstractmethod
+    @abstractmethod
     def check(*args, **kwargs):
         pass
 
@@ -39,45 +46,50 @@ class PlayerConnectingEvent(Event):
     @classmethod
     def check(cls, line):
         try:
-            _cache["name"] = line[line.index(" is connecting...")]
+            cls._cache["name"] = line[line.index(" is connecting...")]
             return True
         except ValueError:
+            cls._cache = {}
             return False
 
 class PlayerConnectEvent(Event):
     @classmethod
     def check(cls, line):
         try:
-            _cache["name"] = line[line.index(" connected\x1b[m")]
+            cls._cache["name"] = line[line.index(" connected\x1b[m")]
             return True
         except ValueError:
+            cls._cache = {}
             return False
 
 class PlayerSpectateEvent(Event):
     @classmethod
     def check(cls, line):
         try:
-            _cache["name"] = line[line.index(" is now spectating\x1b[m")]
+            cls._cache["name"] = line[line.index(" is now spectating\x1b[m")]
             return True
         except ValueError:
+            cls._cache = {}
             return False
 
 class PlayerJoinEvent(Event):
     @classmethod
     def check(cls, line):
         try:
-            _cache["name"] = line[line.index(" is now playing\x1b[m")]
+            cls._cache["name"] = line[line.index(" is now playing\x1b[m")]
             return True
         except ValueError:
+            cls._cache = {}
             return False
 
 class PlayerDisconnectEvent(Event):
     @classmethod
     def check(cls, line):
         try:
-            _cache["name"] = line[line.index(" disconnected\x1b[m")]
+            cls._cache["name"] = line[line.index(" disconnected\x1b[m")]
             return True
         except ValueError:
+            cls._cache = {}
             return False
 
 
@@ -87,29 +99,34 @@ class ChatMsgEvent(Event):
     def check(cls, line):
         temp = cls._chatMsg.match(line)
         if temp:
-            _cache["sender"] = temp[0]
-            _cache["message"] = temp[1]
+            cls._cache["line"] = temp[0]
+            cls._cache["sender"] = temp[1]
+            cls._cache["message"] = temp[2]
             return True
+        else:
+            cls._cache = {}
         return False
 
 class FragEvent(Event):
     _fragMsg = re.compile("^(?:(?:\x1b\[\d\;\d+m)?(.*?)\x1b.*? .*?(?:\x1b\[[\d;m]+)+(.*?)(?:\x1b\[[\d;m]+)+.*'s (.*?) \(near (.*)\)(?:,[A-z\s]+(\d+))?|(?:(?:\x1b\[\d\;\d+m)?(.*?)\x1b.*? .*?(?:\x1b\[[\d;m]+)+(.*?)(?:\x1b\[[\d;m]+)+).*?(\w+) \(near (.*)\))")
-    _cache = {}
     @classmethod
     def check(cls, line):
         temp = cls._fragMsg.match(line)
         if temp:
             temp = tuple(filter(lambda x: x, temp.groups()))
-            _cache["victim"] = temp[0]
-            _cache["attacker"] = temp[1]
-            _cache["weapon"] = temp[2]
-            _cache["location"] = temp[3]
+            cls._cache["line"] = temp[0]
+            cls._cache["victim"] = temp[1]
+            cls._cache["attacker"] = temp[2]
+            cls._cache["weapon"] = temp[3]
+            cls._cache["location"] = temp[4]
             try:
-                _cache["fragstreak"] = temp[4]
+                cls._cache["fragstreak"] = temp[5]
             except IndexError:
-                _cache["fragstreak"] = None
+                cls._cache["fragstreak"] = None
             finally:
                 return True
+        else:
+            cls._cache = {}
         return False
 
 class SuicideEvent(Event):
@@ -133,7 +150,7 @@ class DebugEvent(Event):
     def check(cls, line):
         return True
 
-
 for c in Event.__subclasses__():
+    print(c)
     setattr(c, "_handlers", [])
     setattr(c, "_cache", {})
